283c2a9bb2f6015f51bd65203689cb14ff9b0945
Rijnard van Tonder <rvantonder@gmail.com>
2020-06-18 01:58:17 -0700

    redirect.js

---
+++ website/pages/en/redirect.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Redirect extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>yo</p>
+                </div>
+        );
+    }
+}
+
+module.exports = Redirect;
