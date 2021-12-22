c4fc2fc849f382e3c4bea97c6beec82ab4360e53
Rijnard van Tonder <rvantonder@gmail.com>
2021-12-21 20:31:11 -0700

    deploy from directory (#11)

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-rm -rf CNAME css en pdfs projects projects.html redirect redirect.html index.html blog docs img js sitemap.xml
+# rm -rf CNAME css en pdfs projects projects.html redirect redirect.html index.html blog docs img js sitemap.xml
 cd website
-yarn install
-yarn build
-mv build/comby/* ..
-cd ..
-git add .
-git commit -m "update site"
+yarn --cwd website install && yarn --cwd website build
+# mv build/comby/* ..
+# cd ..
+# git add .
+# git commit -m "update site"
